3fa4ad81e62de90f5a0105ba46c9d89a18e3e3bc
Josseline J <Josseline J@DESKTOP-K7V0K94>
2023-04-12 17:47:26 -0600

    Finalización de la clase 12

--- src/main/java/com/TiendaM_IQ2023/Encriptar.java
+++ src/main/java/com/TiendaM_IQ2023/Encriptar.java
@@ -1,7 +1,13 @@
 package com.TiendaM_IQ2023;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 public class Encriptar {
 
     public static void main(String[] args) {
+        var codigo = new BCryptPasswordEncoder();
+        System.out.println("Juan(123)" + codigo.encode("123"));
+        System.out.println("Rebeca(456)" + codigo.encode("456"));
+        System.out.println("Pedro(789)" + codigo.encode("789"));
     }
 }
